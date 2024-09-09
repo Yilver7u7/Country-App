2bebf1f475a6347b52ebb120262f511340c6b12b
Yilver7u7 <yilversgg@gmail.com>
2024-09-08 20:35:00 -0500

    Add countries.service.ts judt return the Observable

---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/country';
+
+@Injectable({providedIn: 'root'})
+export class ServiceNameService {
+  constructor(private http: HttpClient) { }
+
+  private apiUrl: string = 'https://restcountries.com/v3.1'
+
+  searchCapital( queryString:string ):Observable<Country[]>{
+    const url = `${this.apiUrl}/capital/${queryString}`;
+    return this.http.get<Country[]>( url )
+  }
+
+}
+
+
