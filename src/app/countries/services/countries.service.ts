4341c30de81466b4a08276a9d4ed1bf2c99035df
Yilver7u7 <yilversgg@gmail.com>
2024-09-11 20:44:35 -0500

    Add the render that allow to show the info from any country

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of, tap } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -8,6 +8,18 @@ export class CountriesService {
   constructor(private http: HttpClient) { }
 
   private apiUrl: string = 'https://restcountries.com/v3.1'
+
+
+  searchCountryByAlphaCode( code: string ): Observable<Country | null> {
+
+    const url = `${ this.apiUrl }/alpha/${ code }`;
+
+    return this.http.get<Country[]>( url )
+      .pipe(
+        map( countries => countries.length > 0 ? countries[0]: null ),
+        catchError( () => of(null) )
+      );
+  }
 
   searchCapital( queryString:string ):Observable<Country[]>{
     const url = `${this.apiUrl}/capital/${queryString}`;
